2a68292fdbcc246bdba2912320b206562bfca7c4
vayers <violetlastname@gmail.com>
2023-12-01 11:10:41 -0500

    complete day 1 part 1

---
+++ Day1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.FileReader;
+
+class Day1 {
+    public static void main(String[] args) {
+        Scanner s;
+
+        try {
+            s = new Scanner(new FileReader("input/day1input.txt"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        int total = 0;
+        String line;
+
+        while (s.hasNextLine()) {
+            line = s.nextLine();
+            
+            line = line.replaceAll("[a-zA-Z]+", "");
+            String[] parts = line.split("");
+            
+            line = parts[0] + parts[parts.length - 1];
+            total += Integer.parseInt(line);
+        }
+
+        System.out.println(total);
+    }
+}
